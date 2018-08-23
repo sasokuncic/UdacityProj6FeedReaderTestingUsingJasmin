2698ac708ec73dea7a9b7f5888cd11666dbd170c
sasokuncic <saso.kuncic@gmail.com>
2018-08-23 12:53:14 +0200

    RSS Feeds test: all urls are defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,17 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('all urls are defined', function() {
+            for (let feed of allFeeds) {
+              expect(feed.url).toBeDefined();
+              expect(feed.url.length).not.toBe(0);
+              console.log(feed.url);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
