0b524a1c8651ef214711e4f0fc79cd6f9d5217a6
sasokuncic <saso.kuncic@gmail.com>
2018-08-23 14:21:47 +0200

    The Menu tests and README.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UdacityProj6FeedReaderTestingUsingJasmin
-udacity Front-End Web Developer Nanodegree, Project: Feed Reader Testing with Jasmine
+udacity Front-End Web Developer Nanodegree, Project: Feed Reader Testing with Jasmine. For more details see project's [template and requirements](https://github.com/udacity/frontend-nanodegree-feedreader)
+
+## RSS FeedReader test
++ [Online](https://sasokuncic.github.io/UdacityProj6FeedReaderTestingUsingJasmin/)
+
+## Implementation steps
++ [x] Experimenting with allFeeds: remove all items, using name2 instead of name in feeds, adding new feeds with Slovenian news.
++ [x] Adding "The Menu" tests. Need to remember how to simulate click. See reference.
+
+## References
++ [Check whether the selected element has the specified class or not](https://www.bitdegree.org/learn/jquery-hasclass/)
++ [How to trigger a click on a link using jQuery](https://stackoverflow.com/questions/5811122/how-to-trigger-a-click-on-a-link-using-jquery)
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,19 +50,30 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
-         */
+         */        
+         it("pizza menu is hidden", function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it("pizza menu shown after the first click and hidden after the second one", function() {
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
